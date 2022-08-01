715ea672f38c72279ee9bec568c3f153e51b7858
Daniel Pelajo <dpelajo@coppetec.ufrj.br>
2022-08-01 16:58:56 -0300

    Header done. Locales

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const nextTranslate = require('next-translate')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  ...nextTranslate(),
 }
+
 
 module.exports = nextConfig
